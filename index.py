d389c43213ad68c0e125ce20d4a818b7029e477e
Marusoftware <marusoftware@outlook.jp>
2022-01-15 03:40:26 +0900

    fix many

--- index.py
+++ index.py
@@ -8,13 +8,16 @@ print("Content-type:text/html\n\n")
 cgitb.enable(display=False)
 
 form = cgi.FieldStorage()
-if "id" in form:
-    id = form["id"].value
-else:
-    if not form["mode"].value == "vote":
-        print("No id presented...")
-        sys.exit()
-step = 0 if not "step" in form else int(form["step"].value)
+id = form.getvalue("id", None)
+mode=form.getvalue("mode", "set")
+if id is None and mode != "vote":
+    print("No id presented...")
+    sys.exit()
+try:
+    step = int(form.getvalue("step", 0))
+except:
+    print("step is not an number.")
+    sys.exit()
 
 def require(lst, obj, rt_losts=False):
     lost=[]
@@ -30,12 +33,15 @@ def require(lst, obj, rt_losts=False):
             return False
 
 user=User()
-if not "mode" in form or form["mode"].value == "set":
-    if not "srv_id" in form:
+if mode == "set":
+    import re
+    if not re.fullmatch("[0-9A-Fa-f]+", id):
+        print("Server attack was detected.")
+        sys.exit()
+    srv_id=form.getvalue("srv_id", None)
+    if srv_id is None:
         print("サーバーidが指定されていません。")
         sys.exit()
-    else:
-        srv_id=form["srv_id"].value
     if user.loadvote(srv_id, id)["status"] != "not set":
         print("既に設定されています。再設定するには選挙管理委員権限をもつメンバーに解除してもらってください。")
         sys.exit()
@@ -52,40 +58,39 @@ if not "mode" in form or form["mode"].value == "set":
         output+='一度設定すると取り消すには選挙管理委員権限が必要です。<br /><input type="submit" /></form>'
     else:
         if require(["name","date","time","vote_mode", "users", "index"],form):
-            name=form["name"].value
-            date=form["date"].value.split("-")
-            time=form["time"].value.split(":")
+            name=form.getvalue("name")
+            date=form.getvalue("date").split("-")
+            time=form.getvalue("time").split(":")
             dt=datetime.datetime(year=int(date[0]),month=int(date[1]),day=int(date[2]),hour=int(time[0]),minute=int(time[1]))
-            vote_mode=int(form["vote_mode"].value)
+            vote_mode=int(form.getvalue("vote_mode"))
             users=form.getlist("users")
             if vote_mode==1:
-                index=form["index"].value.split(",")
+                index=form.getvalue("index").split(",")
             user.setvote(srv_id, id,users,vote_mode,name,dt,index)
             output=f'正常に投票を作成できました。<br />この投票のidは"{id}"です。<br />"!start_vote {id}"で投票を開始してください。'
         else:
             output="何か問題が起きたようです。設定に必要なデータが足りません。<br />"
             output+=str(require(["name","date","time"],form,rt_losts=True))
-elif form["mode"].value == "vote":
+elif mode == "vote":
     if step == 0:
-        output='<form method="post" action="./" enctype="application/x-www-form-urlencoded"><input type="hidden" name="mode" value="vote"><input type="hidden" name="step" value="1">'
+        output='<form method="post" action="" enctype="application/x-www-form-urlencoded"><input type="hidden" name="mode" value="vote"><input type="hidden" name="step" value="1">'
         output+='<label>投票トークン:<input type="text" name="token"/></label><br /><input type="submit" /></form>'
     elif step == 1:
-        if require(["token"],form):
-            token=form["token"].value
-            #id = user.token2id(token)
-            #temp = user.loadvote(id)
-            temp=user.loadvote_token(token)
-            output=f'<form method="post" action="./" enctype="application/x-www-form-urlencoded"><input type="hidden" name="mode" value="vote" /><input type="hidden" name="token" value="{token}" /><input type="hidden" name="step" value="2">'
-            if temp["mode"] == 1:
-                for index in temp["index"]:
-                    output+=f'<label><input type="radio" name="vote" value="{index}"/>{index}</label><br />'
-            else:
-                print("未実装です。")
-                sys.exit()
-            output+='<input type="submit" /></form>'
+        token=form.getvalue("token", None)
+        if token is None:
+            print("投票トークンが指定されていません")
+            sys.exit()
+        #id = user.token2id(token)
+        #temp = user.loadvote(id)
+        temp=user.loadvote_token(token)
+        output=f'<form method="post" action="" enctype="application/x-www-form-urlencoded"><input type="hidden" name="mode" value="vote" /><input type="hidden" name="token" value="{token}" /><input type="hidden" name="step" value="2">'
+        if temp["mode"] == 1:
+            for index in temp["index"]:
+                output+=f'<label><input type="radio" name="vote" value="{index}"/>{index}</label><br />'
         else:
-            output="何か問題が起きたようです。投票に必要なデータが足りません。<br />"
-            output+=str(require(["token"],form,rt_losts=True))
+            print("未実装です。")
+            sys.exit()
+        output+='<input type="submit" /></form>'
     else:
         if require(["token","step","vote"],form):
             if user.vote_token(form["token"].value, form["vote"].value):
